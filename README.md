fbf6095d1bf0c6dc2967103ffcb1ad2787c19522
Adam Zimmermann <adam@adamzimmermann.com>
2020-11-17 15:53:03 -0600

    Initial functionality.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # acquia-db-dump-s3-sync-action
-Acquia Database Backup and S3 Sync
+
+GitHub Action to download database dumps from Acquia and copy them to S3.
+
+## Usage
+
+```yaml
+- uses: chromatichq/acquia-backup-sync-action@v1
+  with:
+    acquia_project: 'example-project'  # required.
+    aws_s3_bucket: 'bucket-name'  # required.
+  env:
+    ACQUIA_PRIVATE_KEY: ${{ secrets.ACQUIA_PRIVATE_KEY }}  # required.
+    AWS_ACCESS_KEY_ID: ${{ secrets.AWS_ACCESS_KEY_ID }}  # required.
+    AWS_SECRET_ACCESS_KEY: ${{ secrets.AWS_SECRET_ACCESS_KEY }}  # required.
+    AWS_DEFAULT_REGION: 'us-west-2' # required.
+```
---
+++ entrypoint.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -l
+
+php --version
+aws --version
+
+# Set the ssh key name.
+SSH_KEY_NAME="acquia"
+
+# @todo Re-enable this.
+sed -i 's/#   StrictHostKeyChecking ask.*/StrictHostKeyChecking accept-new/' /etc/ssh/ssh_config
+
+# Create the ssh key.
+echo $ACQUIA_PRIVATE_KEY > "~/.ssh/$SSH_KEY_NAME"
+
+# @todo Remove this.
+# INPUT_ACQUIA_PROJECT="imprivata8"
+# INPUT_ACQUIA_ENVIRONMENT="prod"
+# INPUT_AWS_S3_BUCKET="acquia-test"
+
+# Get a list of available Acquia databases.
+ACQUIA_DATABASES=$(ssh -i ~/.ssh/"$SSH_KEY_NAME" "$INPUT_ACQUIA_PROJECT"."$INPUT_ACQUIA_ENVIRONMENT"@"$INPUT_ACQUIA_PROJECT".ssh.prod.acquia-sites.com ls /mnt/files/"$INPUT_ACQUIA_PROJECT"/backups/)
+# Get a list of databases already synced to S3.
+S3_DATABASES=$(aws s3 ls s3://"$INPUT_AWS_S3_BUCKET" | awk '{print $4}')
+
+# Build a list of files in Acquia, but not in S3 that should be copied over.
+FILE_DIFF=$(diff <(echo "$ACQUIA_DATABASES") <(echo "$S3_DATABASES") --suppress-common-lines | grep "$ACQUIA_DATABASES" | grep -vE 'on-demand' | awk '{print $2}')
+
+echo ""
+echo 'Acquia Databases:'
+echo "$ACQUIA_DATABASES"
+
+echo ""
+echo 'S3 Databases'
+echo "$S3_DATABASES"
+
+echo ""
+echo "FILE_DIFF"
+echo "$FILE_DIFF"
+
+exit 0
+
+# Loop through the new files and download them from Acquia, then upload to S3.
+while IFS='\n' read -ra DATABASE_FILE_NAME; do
+  echo "Downloading $DATABASE_FILE_NAME from Acquia."
+  scp -i ~/.ssh/"$SSH_KEY_NAME" "$INPUT_ACQUIA_PROJECT"."$INPUT_ACQUIA_ENVIRONMENT"@"$INPUT_ACQUIA_PROJECT".ssh.prod.acquia-sites.com:/mnt/files/"$INPUT_ACQUIA_PROJECT"/backups/"$DATABASE_FILE_NAME" ./
+  echo "Uploading $DATABASE_FILE_NAME to the $INPUT_AWS_S3_BUCKET S3 bucket."
+  aws s3 cp "$DATABASE_FILE_NAME" "s3://$INPUT_AWS_S3_BUCKET"
+done <<< "$FILE_DIFF"
