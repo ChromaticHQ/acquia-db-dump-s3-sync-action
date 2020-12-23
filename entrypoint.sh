129af67066c600dda5f08581963f9de670fc1d48
adamzimmermann <adam@adamzimmermann.com>
2020-12-23 13:31:03 -0600

    Refactor diff logic.

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,20 +11,18 @@ ssh-keyscan -t rsa "$INPUT_ACQUIA_PROJECT.ssh.prod.acquia-sites.com" >> ~/.ssh/k
 
 # Get a list of available Acquia databases.
 ACQUIA_DATABASES=$(ssh -i ~/.ssh/"$SSH_KEY_NAME" "$INPUT_ACQUIA_PROJECT"."$INPUT_ACQUIA_ENVIRONMENT"@"$INPUT_ACQUIA_PROJECT".ssh.prod.acquia-sites.com ls /mnt/files/"$INPUT_ACQUIA_PROJECT"/backups/)
-# Get a list of databases already synced to S3.
-S3_DATABASES=$(aws s3 ls s3://"$INPUT_AWS_S3_BUCKET" | awk '{print $4}')
-
-# Build a list of files in Acquia, but not in S3 that should be copied over.
-FILE_DIFF=$(diff <(echo "$ACQUIA_DATABASES") <(echo "$S3_DATABASES") --suppress-common-lines | grep "$ACQUIA_DATABASES" | grep '<' | grep -vE 'on-demand' | awk '{print $2}')
-
 echo ""
-echo 'Acquia Databases:'
+echo "Acquia Databases:"
 echo "$ACQUIA_DATABASES"
 
+# Get a list of databases already synced to S3.
+S3_DATABASES=$(aws s3 ls s3://"$INPUT_AWS_S3_BUCKET" | awk '{print $4}')
 echo ""
-echo 'S3 Databases:'
+echo "S3 Databases:"
 echo "$S3_DATABASES"
 
+# Build a list of files in Acquia, but not in S3 that should be copied over.
+FILE_DIFF=$(comm -23 <(echo "$ACQUIA_DATABASES" | sort) <(echo "$S3_DATABASES" | sort) | grep -v on-demand)
 echo ""
 echo "Files to Upload:"
 echo "$FILE_DIFF"
@@ -33,8 +31,8 @@ echo "$FILE_DIFF"
 while IFS=$'\n' read -ra DATABASE_FILE_NAME; do
   echo ""
   echo "Downloading $DATABASE_FILE_NAME from Acquia."
-  # scp -i ~/.ssh/"$SSH_KEY_NAME" "$INPUT_ACQUIA_PROJECT"."$INPUT_ACQUIA_ENVIRONMENT"@"$INPUT_ACQUIA_PROJECT".ssh.prod.acquia-sites.com:/mnt/files/"$INPUT_ACQUIA_PROJECT"/backups/"$DATABASE_FILE_NAME" ./
+  scp -i ~/.ssh/"$SSH_KEY_NAME" "$INPUT_ACQUIA_PROJECT"."$INPUT_ACQUIA_ENVIRONMENT"@"$INPUT_ACQUIA_PROJECT".ssh.prod.acquia-sites.com:/mnt/files/"$INPUT_ACQUIA_PROJECT"/backups/"$DATABASE_FILE_NAME" ./
   echo "Uploading $DATABASE_FILE_NAME to the $INPUT_AWS_S3_BUCKET S3 bucket."
-  # aws s3 cp "$DATABASE_FILE_NAME" "s3://$INPUT_AWS_S3_BUCKET"
-  # rm "$DATABASE_FILE_NAME"
+  aws s3 cp "$DATABASE_FILE_NAME" "s3://$INPUT_AWS_S3_BUCKET"
+  rm "$DATABASE_FILE_NAME"
 done <<< "$FILE_DIFF"
